33269c8d5adc2c11dd86481a359cb8eb2dfc5798
agneszhrn <agneszhrn@gmail.com>
2021-05-31 00:55:44 +0700

    update home, list kost

--- kost/application/views/home/index.php
+++ kost/application/views/home/index.php
@@ -1,11 +1,31 @@
 <div class="container">
-  <div class="row">
-    <div class="col-md-7">
-      <h1 class="mt-5 welcome-text">TELKOM PURBALINGGA PSB</h1>
-      <p class="mt-3 welcome-text">adalah Web Aplikasi yang dirancang untuk karyawan Telkom Purbalingga dalam mendata calon pelanggan dengan mudah. Calon pelanggan Pasang Baru layanan IndiHome yang terdata dalam web ini akan melakukan prosedur selanjutnya dengan pengecekkan lokasi oleh teknisi Telkom Purbalingga. Web ini dikhususkan untuk Divisi PSB yang mendapatkan data pelanggan dari sales Telkom Purbalingga. </p>
-    </div>
-    <div class="col-md-5" style="margin-top: 30px">
-      <img src="<?= base_url('assets/home.jpeg')?>" alt="" width="500">
-    </div>
-  </div>
+	<!-- <div class="row"> -->
+		<div style="text-align: center; margin-top: 250px; margin-bottom: 150px">
+			<h1 style="font-size: 40px; font-weight: bold;">Cari Kos?</h1>
+			<p style="font-size: 20px; font-weight: bold;">Kini lebih mudah dengan Kost Hunter!</p>
+			<button class="btn btn-primary" style="background-color: #35AAB1">Cari sekarang -></button>
+		</div>
+		<div class="row" style="text-align: center">
+			<div class="card col-sm-4" style="width: 8rem; margin-left: 5px; margin-right: 5px">
+				<div class="card-body">
+					<h5 class="card-title">Menginap dengan Aman</h5>
+					<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+					<p class="card-text">Informasi lingkungan kosan sesuai dengan apa yang tertera pada website.</p>
+				</div>
+			</div>
+			<div class="card col-sm-4" style="width: 8rem; margin-left: 5px; margin-right: 5px">
+				<div class="card-body">
+					<h5 class="card-title">Pembayaran Aman</h5>
+					<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+					<p class="card-text">Kami menggunakan pembayaran via tranfer bank untuk mencegah adanya penipuan.</p>
+				</div>
+			</div>
+			<div class="card col-sm-4" style="width: 8rem; margin-left: 5px; margin-right: 5px">
+				<div class="card-body">
+					<h5 class="card-title">Rumah Asli</h5>
+					<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+					<p class="card-text">Foto pada aplikasi ini adalah foto asli dari pemilik kosan.</p>
+				</div>
+			</div>
+		</div>
 </div>
--- kost/application/views/templates/header.php
+++ kost/application/views/templates/header.php
@@ -1,35 +1,61 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?= base_url('assets/css/style.css')?>">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
-    <title><?php echo $judul; ?></title>
-  </head> 
-  <body>
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+		<link rel="stylesheet" href="<?= base_url('assets/css/style.css')?>">
+		<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+		<title><?php echo $judul; ?></title>
+	</head> 
+	
+	<body>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+		<a class="navbar-brand mb-0" style="font-size: 25px; font-weight: bold; color: #35AAB1" href="#">KOST HUNTER</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item active">
+				<a class="nav-link" href="<?=base_url('home/')?>">Home</a>
+				</li>
+				<li class="nav-item active">
+				<a class="nav-link" href="<?=base_url('Kost/')?>">List Kost</a>
+				</li>
+				<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Hai, <?=$sesi['user']['namaPencari']?>
+				</a>
+				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+					<a class="dropdown-item" href="#">Profile</a>
+					<a class="dropdown-item" href="<?=base_url('home/logout')?>" onclick="return confirm('Logout?');">Logout</a>
+				</div>
+				</li>
+			</ul>
+		</div>
+		</nav>
 
-<nav class="navbar navbar-expand-lg navbar-light">
-  <div class="container">
-    <a href="<?=base_url('home/')?>">
-      <img src="<?=base_url("assets/logo-telkom.png");?>" alt="logo-telkom" width=100>
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav ml-5">
-        <a class="nav-item nav-link mx-3" href="<?=base_url('home/')?>">Home</a>
-        <a class="nav-item nav-link mx-3" href="<?=base_url('Kost/')?>">List Kost</a>
-        <a class="nav-item nav-link mx-3 akun" href="<?=base_url('home/logout')?>" onclick="return confirm('Logout?');">Hai, 
-          <?=$sesi['user']['namaPencari']?>
-        </a>
-      </div>
-    </div>
-  </div>
-</nav>
+		<!-- <nav class="navbar navbar-expand-lg navbar-light">
+		<div class="container">
+			<a href="<?=base_url('home/')?>">
+				<img src="<?=base_url("assets/logo-telkom.png");?>" alt="logo-telkom" width=100>
+			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+				<div class="navbar-nav ml-5">
+				<a class="nav-item nav-link mx-3" href="<?=base_url('home/')?>">Home</a>
+				<a class="nav-item nav-link mx-3" href="<?=base_url('Kost/')?>">List Kost</a>
+				<a class="nav-item nav-link mx-3 akun" href="<?=base_url('home/logout')?>" onclick="return confirm('Logout?');">Hai, 
+					<?=$sesi['user']['namaPencari']?>
+				</a>
+				</div>
+			</div>
+		</div>
+		</nav> -->
